ef6cb695abb429f0e81830eaca8a53cea791ba1e
pavankumardasireddy <pavankumar.dasireddy@gmail.com>
2019-08-15 01:21:42 +0530

    Finished client task

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+const url = 'http://localhost:3002'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    process: '-1',
+    key: '',
+    value: '',
+    data: null
+  }
+
+
+  getProcessJson = (process) => {
+    if (process === '-1') {
+      return false;
+    }
+    fetch(` ${url}/getEnvironment/${process}`)
+      .then((data) => data.json())
+      .then((res) => this.setState({ data: res }));
+  };
+
+  updateProcessJson = (process, key, value) => {
+    if (process === '-1') {
+      return false;
+    }
+    fetch(`${url}/setEnvironment/${process}/${key}/${value}`)
+      .then((data) => data.json())
+      .then((res) => this.setState({ data: res }));
+  };
+
+
+  render() {
+    let { data, process, key, value } = this.state;
+    return (
+      <div>
+        <div style={{ padding: '10px' , textAlign: 'center'}}>
+          <select value={process} onChange={(event) => this.setState({ process: event.target.value })}>
+            <option value="-1">Select process</option>
+            <option value="P1">P1</option>
+            <option value="P2">P2</option>
+          </select>
+          <button type="button" class="btn btn-primary btn-sm" onClick={() => this.getProcessJson(process)}>GET PROCESS DATA</button>
+        </div>
+        <ul style={{ padding: '0px auto' }}>
+          {data && Object.keys(data).map((dat) => (
+                <li style={{ padding: '10px' }} key={dat}>
+                  <span style={{ color: 'gray' }}> Key: </span> {dat} <br />
+                  <span style={{ color: 'gray' }}> Value: </span>
+                  {(data)[dat]}
+                </li>
+              ))}
+        </ul>
+        <div style={{ padding: '10px' , textAlign: 'center'}}>
+          <select value={process} onChange={(event) => this.setState({ process: event.target.value })}>
+            <option value="-1">Select process</option>
+            <option value="P1">P1</option>
+            <option value="P2">P2</option>
+          </select>
+          <input
+            type="text"
+            value={key}
+            style={{ width: '200px' }}
+            onChange={(e) => this.setState({ key: e.target.value })}
+            placeholder="New key to update here"
+          />
+          <input
+            type="text"
+            value={value}
+            style={{ width: '200px' }}
+            onChange={(e) => this.setState({ value: e.target.value })}
+            placeholder="New value to update here"
+          />
+          <button
+            onClick={() =>
+              this.updateProcessJson(process, key, value)
+            }
+          >
+            UPDATE
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
